c79d3567cd6ae96cefee575dfbef98e74257dd81
B0YnextDoor <ppetrov228@bk.ru>
2022-07-04 17:03:48 +0300

    gey

---
+++ main.py
@@ -0,0 +1,34 @@
+ttt = "5359762951:AAGZCgSwJk3GqkQOTtLxUs_OheOiWyIktR4"
+Group_ID=1
+import logging
+import filters
+
+from aiogram import Bot, Dispatcher, executor, types
+from filters import IsAdminFilter
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=ttt)
+dp = Dispatcher(bot)
+
+dp.filters_factory.bind(IsAdminFilter)
+
+@dp.message_handler(content_types=['new chat members'])
+async def on_user_joined(message: types.Message):
+    await message.delete()
+
+@dp.message_handler(is_admin=True, commands=['ban'], commands_prefix='!/')
+async def cmd_ban(message: types.Message):
+    if not message.reply_to_message :
+        await message.reply('Эта команда должна быть ответом на сообщение!')
+        return
+    await message.bot.delete_message(Group_ID, message.message_id)
+    await message.bot.kick_chat_member(chat_id=Group_ID, user_id=message.reply_to_message.from_user.id)
+
+    await message.reply_to_message.reply('БАН НАХУЙ!')
+@dp.message_handler()
+async def filter_messages(message: types.Message) :
+    if 'сука' or 'блядь' in message.text:
+        await message.delete()
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates= True)
