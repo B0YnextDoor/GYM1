449d92bcb87a518807688aba875eef3f45dd43f6
GRigorii <aleksnermer@gmail.com>
2022-07-05 13:10:26 +0300

    паша пидор

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-ttt = "5359762951:AAGZCgSwJk3GqkQOTtLxUs_OheOiWyIktR4"
-Group_ID=1
 import logging
 import filters
-
 from aiogram import Bot, Dispatcher, executor, types
 from filters import IsAdminFilter
-logging.basicConfig(level=logging.INFO)
+import configparser
 
-bot = Bot(token=ttt)
+conf = configparser.ConfigParser()
+conf.read('config.ini')
+logging.basicConfig(level=logging.INFO)
+token = conf['bot']['token']
+print(token)
+
+bot = Bot(token)
 dp = Dispatcher(bot)
 
 dp.filters_factory.bind(IsAdminFilter)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await bot.send_message(message.chat.id,'sadasd')
+
 
 @dp.message_handler(content_types=['new chat members'])
 async def on_user_joined(message):
@@ -18,19 +27,21 @@ async def on_user_joined(message):
 
 @dp.message_handler( commands=['ban'], commands_prefix='!/')
 async def cmd_ban(message):
-    # if message.id == 'твой id':
-    #     pass
-    if not message.reply_to_message :
-        await message.reply('Эта команда должна быть ответом на сообщение!')
-        return
-    await message.bot.delete_message(Group_ID, message.message_id)
-    await message.bot.kick_chat_member(chat_id=Group_ID, user_id=message.reply_to_message.from_user.id)
+    if message.id == '469522174':
+       
+        if not message.reply_to_message :
+            await message.reply('Эта команда должна быть ответом на сообщение!')
+            return
+        await message.bot.delete_message(-1001204842425, message.message_id)
+        await message.bot.kick_chat_member(-1001204842425, user_id=message.reply_to_message.from_user.id)
 
     await message.reply_to_message.reply('БАН НАХУЙ!')
+
+
 @dp.message_handler()
 async def filter_messages(message: types.Message) :
     if 'сука' or 'блядь' in message.text:
         await message.delete()
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates= True)
+    executor.start_polling(dp)
